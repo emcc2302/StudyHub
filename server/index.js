c21ca61eef77c59783add0e19bb2338693caa39f
Soriful Islam Sk <sorifulislamsk1@gmail.com>
2026-01-01 20:43:02 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -36,6 +36,6 @@ app.use("/api", courseRoutes);
 app.use("/api", adminRoutes);
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  // console.log(`Server is running on http://localhost:${port}`);
   connectDb();
 });
